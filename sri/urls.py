54c5f4876d45817b31bb7367c4358734628a6933
JayVillain <saipi3825@gmail.com>
2026-01-22 19:44:06 +0000

    Jan 23, 2026, 2:44 AM

--- sri/urls.py
+++ sri/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from .views import daftar, sukses
+
+app_name = 'sri'
 
 urlpatterns = [
-    path('pendaftaran/', lambda request: None),
+    path('pendaftaran/', daftar, name='daftar'),
+    path('pendaftaran/sukses/', sukses, name='sukses'),
 ]
