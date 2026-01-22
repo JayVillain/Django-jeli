3605abc391bb0d8552ea829d9701a843924498a4
JayVillain <saipi3825@gmail.com>
2026-01-22 20:15:28 +0000

    Jan 23, 2026, 3:15 AM

--- sri/admin.py
+++ sri/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Negara, Provinsi, Kabupaten, Pendaftar
+from .models import (
+    Negara, Provinsi, Kabupaten,
+    Kecamatan, Desa, Pendaftar
+)
 
 admin.site.register(Negara)
 admin.site.register(Provinsi)
 admin.site.register(Kabupaten)
+admin.site.register(Kecamatan)
+admin.site.register(Desa)
 admin.site.register(Pendaftar)
