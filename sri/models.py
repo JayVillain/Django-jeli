6610ca20eeef8c99c0c39d79b2e0cc87a9f64a22
JayVillain <saipi3825@gmail.com>
2026-01-22 19:52:41 +0000

    Jan 23, 2026, 2:52 AM

--- sri/models.py
+++ sri/models.py
@@ -6,3 +6,24 @@ class Pendaftar(models.Model):
 
     def __str__(self):
         return self.nama
+class Negara(models.Model):
+    nama = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nama
+
+
+class Provinsi(models.Model):
+    negara = models.ForeignKey(Negara, on_delete=models.CASCADE)
+    nama = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nama
+
+
+class Kabupaten(models.Model):
+    provinsi = models.ForeignKey(Provinsi, on_delete=models.CASCADE)
+    nama = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nama
