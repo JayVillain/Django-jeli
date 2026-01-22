8691dc83ba71d01ff4583789eba587d2f335bff4
JayVillain <saipi3825@gmail.com>
2026-01-22 19:53:14 +0000

    Jan 23, 2026, 2:53 AM

--- sri/models.py
+++ sri/models.py
@@ -3,9 +3,13 @@ from django.db import models
 class Pendaftar(models.Model):
     nama = models.CharField(max_length=100)
     email = models.EmailField()
+    negara = models.ForeignKey(Negara, on_delete=models.SET_NULL, null=True)
+    provinsi = models.ForeignKey(Provinsi, on_delete=models.SET_NULL, null=True)
+    kabupaten = models.ForeignKey(Kabupaten, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.nama
+
 class Negara(models.Model):
     nama = models.CharField(max_length=100)
 
