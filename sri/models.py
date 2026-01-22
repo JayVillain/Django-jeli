dec79d77c5bd2124a7ad4b7a880ba7c87f64bf2b
JayVillain <saipi3825@gmail.com>
2026-01-22 19:54:14 +0000

    Jan 23, 2026, 2:54 AM

--- sri/models.py
+++ sri/models.py
@@ -1,14 +1,5 @@
 from django.db import models
 
-class Pendaftar(models.Model):
-    nama = models.CharField(max_length=100)
-    email = models.EmailField()
-    negara = models.ForeignKey(Negara, on_delete=models.SET_NULL, null=True)
-    provinsi = models.ForeignKey(Provinsi, on_delete=models.SET_NULL, null=True)
-    kabupaten = models.ForeignKey(Kabupaten, on_delete=models.SET_NULL, null=True)
-
-    def __str__(self):
-        return self.nama
 
 class Negara(models.Model):
     nama = models.CharField(max_length=100)
@@ -28,6 +19,17 @@ class Provinsi(models.Model):
 class Kabupaten(models.Model):
     provinsi = models.ForeignKey(Provinsi, on_delete=models.CASCADE)
     nama = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nama
+
+
+class Pendaftar(models.Model):
+    nama = models.CharField(max_length=100)
+    email = models.EmailField()
+    negara = models.ForeignKey(Negara, on_delete=models.SET_NULL, null=True)
+    provinsi = models.ForeignKey(Provinsi, on_delete=models.SET_NULL, null=True)
+    kabupaten = models.ForeignKey(Kabupaten, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.nama
